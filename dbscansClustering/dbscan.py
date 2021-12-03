6f6bbc738bbe0d681e14e770481aad4673772f9e
Mayra Salazar <mayrasalazar@unitec.edu>
2021-12-02 20:52:41 -0600

    dbscans

--- dbscansClustering/dbscan.py
+++ dbscansClustering/dbscan.py
@@ -1,27 +1,27 @@
 import sys
 import matplotlib.pyplot as mplt
 import numpyPandas
-from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 
 
 
 def main():
-    num = None
+    eps = None
+    mins = None
     dataSet = None
     try:
         dataSet = sys.argv[1]
-        num = float(sys.argv[2])
+        eps = float(sys.argv[2])
+        mins= float(sys.argv[3])
     except:
         print("Error at obtaining data: Provide arg1 as dataset and arg2 as clusters")
         sys.exit(1)
 
     allSet = numpyPandas.getArray(dataSet)
-    kmean = KMeans(n_clusters=num)
-    kmean.fit(allSet)
-    kclusters = DBSCAN(eps=3, min_samples=2).fit(allSet)
+    dbscan = DBSCAN(eps=eps, min_samples=mins)
+    dbscan.fit(allSet)
     x,y = allSet[:, 0] , allSet[:, 1]
-    mplt.scatter(x, y, c=kclusters.labels_)
+    mplt.scatter(x, y, c=dbscan.labels_)
     mplt.show()
 
     
